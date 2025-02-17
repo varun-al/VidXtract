3b3076a616309ed29a025827fcfb4383d4fff210
varun-al <varunal2003@gmail.com>
2025-02-17 10:36:51 +0530

    donwload page

---
+++ frontend/src/pages/DownloadPage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Sidebar from "../component/Sidebar"; // Ensure this path is correct
+
+function DownloadPage() {
+    return (
+        <div className="app-container">
+            <Sidebar />
+            <div className="container">
+                <h1 style={{ color: "white", textAlign: "center" }}>Download Page</h1>
+                <p style={{ color: "white", textAlign: "center" }}>In development</p>
+            </div>
+        </div>
+    );
+}
+
+export default DownloadPage;
